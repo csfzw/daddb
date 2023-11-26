d8c14b2b02c136f7bf3e79fe2f8d55dd9cf88651
csfzw <csfzw@qq.com>
2023-11-26 11:19:18 +0800

    init

---
+++ daddb-server/src/main/java/com/fzw/daddb/pojo/Token.java
@@ -0,0 +1,11 @@
+package com.fzw.daddb.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Token {
+    private String id;
+    private String sid;
+}
